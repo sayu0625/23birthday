51efcdf1a98cd769352f5f67ffd822c3eb445fb7
sayu0625 <sayu06250102@gmail.com>
2026-06-29 18:18:55 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
 const app = document.getElementById("app");
 
 app.innerHTML = `
+
+<div class="hero">
+
+<div class="wave"></div>
+
+<div class="content">
+
 <h1>${APP.title}</h1>
 
-<h2>Welcome ${APP.name}</h2>
+<h2>Welcome, ${APP.name}</h2>
 
-<p>${APP.trip}</p>
+<p>
+
+今日は君のためだけに用意した
+
+特別な旅。
+
+</p>
+
+<p>${APP.date}</p>
+
+<button id="startBtn">
+
+START JOURNEY
+
+</button>
+
+</div>
+
+</div>
+
 `;
