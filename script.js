ae2495ef7e3cbbce3704b92b26d1e57001df27cb
sayu0625 <sayu06250102@gmail.com>
2026-06-29 18:37:30 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
 const app = document.getElementById("app");
 
+// カウントダウン
+const today = new Date();
+const trip = new Date(APP.startDate);
+
+today.setHours(0,0,0,0);
+trip.setHours(0,0,0,0);
+
+const diff = trip - today;
+const days = Math.ceil(diff / (1000*60*60*24));
+
+let countdown;
+
+if(days > 0){
+
+    countdown = `あと ${days} 日`;
+
+}else if(days === 0){
+
+    countdown = "🎉 Today!!";
+
+}else{
+
+    countdown = "旅行中";
+
+}
+
+// トップ画面
 app.innerHTML = `
 
 <div class="hero">
@@ -12,25 +39,21 @@ app.innerHTML = `
 
 <h2>Welcome, ${APP.name}</h2>
 
+<h3>${countdown}</h3>
+
 <p>
 
-駿真の23歳の誕生日をお祝いする旅に行こう！
+今日は君のためだけに用意した
+
+特別な旅。
 
 </p>
 
-<p>${APP.date}</p>
-
-const today = new Date();
-
-const trip = new Date(APP.startDate);
-
-const diff = trip - today;
-
-const days = Math.ceil(diff / (1000 * 60 * 60 * 24));
+<p>${APP.tripDate}</p>
 
 <button id="startBtn">
 
-START JOURNEY
+旅をはじめる
 
 </button>
 
